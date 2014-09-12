bb7fe1c6db69f1236973692ec0ddfd77dfa2a46e
Martin Tu <m.turetschek@gmx.de>
2014-09-12 21:50:00 +0200

    assignment (=), equals(==) and unequals(!=) operator for color_t

--- ledPanel/src/colorManipulation.h
+++ ledPanel/src/colorManipulation.h
@@ -12,21 +12,46 @@
 
 struct color_t
 {
-	uint8_t red;
-	uint8_t green;
-	uint8_t blue;
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+
+        color_t& operator=(const color_t& a)
+        {
+            red = a.red;
+            green = a.green;
+            blue = a.blue;
+            return *this;
+        }
+
+        color_t(uint8_t red=0, uint8_t green=0, uint8_t blue = 0)
+        : red(red), green(green), blue(blue)
+        {
+        }
+
+        bool operator==(color_t a) const
+        {
+            if (a.red == red && a.green == green && a.blue == blue)
+                return true;
+            return false;
+        }
+
+        bool operator!=(color_t a) const
+        {
+            if (a.red != red || a.green != green || a.blue != blue)
+                return true;
+            return false;
+        }
 };
 
-static const color_t color_black 		= {  0,  0,  0};
-static const color_t color_dark_grey	= { 20, 20, 20};
-static const color_t color_dark_blue 	= {  0,  0, 20};
+static const color_t color_black = {0, 0, 0};
+static const color_t color_dark_grey = {20, 20, 20};
+static const color_t color_dark_blue = {0, 0, 20};
 
 class ColorMan
 {
-public:
-	static color_t wheel(uint8_t wheelPos);
+    public:
+        static color_t wheel(uint8_t wheelPos);
 };
-
-
 
 #endif /* COLORMANIPULATION_H_ */
